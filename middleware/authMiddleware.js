87d3f9cac615cbde173fe080cc1e7332cc3a9dc4
Aryan <aryanprajapati9667@gmail.com>
2024-07-02 19:21:33 +0530

    ..

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel')
 
 const isAuthenticated = async(req, res,next)=>{
     try {
-        const token = req.cookies.token1
+        const token = req.cookies.token
         if(!token){
             return res.status(401).json({msg:'No token, authorization denied'})
         }
