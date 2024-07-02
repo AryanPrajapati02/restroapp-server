046c9f7ae12c1e235b2320db54c5161d25bbbfb7
Aryan <aryanprajapati9667@gmail.com>
2024-07-02 18:17:52 +0530

    .

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const corsOption = {
     origin: function (origin, callback) {
         if (!origin) return callback(null, true); // Allow non-browser requests
     
-        const allowedDomain =/^http:\/\/([a-zA-Z0-9-]+\.)?mykitab\.live(:[0-9]+)?$/
+        const allowedDomain =/^https:\/\/([a-zA-Z0-9-]+\.)?mykitab\.live(:[0-9]+)?$/
 ;
         if (allowedDomain.test(origin)) {
           callback(null, true);
